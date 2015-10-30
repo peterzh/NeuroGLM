11e840ef53415dc9951301d17c1e8b6d217aa630
Peter Zatka-Haas <p.zatka@ucl.ac.uk>
2015-10-30 17:25:22 +0000

    added laserGLM

---
+++ laserGLM.m
@@ -0,0 +1,17 @@
+classdef laserGLM < GLM
+    properties
+    end
+    
+    methods
+        function obj = laserGLM(expRef)
+            obj@GLM(expRef);
+            L=load(dat.expFilePath(expRef, 'laserManip', 'm'));
+            obj.data.laser = L.laserCoordByTrial;
+        end
+        
+        function obj = fit(obj,cv_flag)
+            obj.data = getrow(obj.data,isnan(obj.data.laser(:,1)));
+            obj=fit@GLM(obj,cv_flag);
+        end
+    end
+end
